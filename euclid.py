de65b1aea5b148a554a79f52f2c0cb0864c7e153
964293341 <58647665+964293341@users.noreply.github.com>
2020-01-28 22:00:44 +0800

    Update euclid.py

--- euclid.py
+++ euclid.py
@@ -1,12 +1,11 @@
 def eu_count(a, b):
-    '''测量'''
+    '''测量measure'''
     if a < b:
         m = b
         n = a
     else:
         m = a
         n = b
-    
     x = 0
     while x == 0:
         c = m - n
@@ -16,12 +15,14 @@ def eu_count(a, b):
             return c
 ###
 def z_num(a):
+	'''判整is it an integer'''
     if eu_count(a, 1) == 0:
         return True
     else:
         return False
 ###
 def gcd(a, b):
+	'''最大公因数greatest common factor'''
     if z_num(a) == True and z_num(b) == True:
         if a > b:
             m = a
@@ -43,20 +44,23 @@ def gcd(a, b):
         print('type_error')
         pass
 ###
-def mass_2num(a, b):
+def prime_2num(a, b):
+	'''判互质are them coprime'''
     if gcd(a, b) == 1:
         return True
     else:
         return False
 ###
 def prop_2num(a, b):
-    if mass_2num(a, b) == True:
+	'''最简数对simplest num_pair'''
+    if prime_2num(a, b) == True:
         return(a, b)
     else:
         c = gcd(a, b)
         return(a / c, b / c)
 ###
 def lcm(a, b):
+	'''最小公倍数least common multiple'''
     c = prop_2num(a, b)[0]
     return b * c
 
